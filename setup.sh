f4b0c537a50e26a49c70353bac4a0764d90253dd
raananelefant <58908387+raananelefant@users.noreply.github.com>
2021-11-21 16:35:27 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/data/data/com.termux/files/usr/bin/bash
 
-pkg install -y python mpv termux-api
+pkg install -y python mpv termux-api curl
 chmod a+x ./*.sh
 curl -L https://yt-dl.org/downloads/latest/youtube-dl -o /data/data/com.termux/files/usr/bin/youtube-dl
 ./youtube-dl-fix.sh
