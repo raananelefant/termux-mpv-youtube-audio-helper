af7ac5464d5165957bec60a20dd39a2ca80252d6
raananelefant <58908387+raananelefant@users.noreply.github.com>
2021-11-21 16:26:46 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,1 +1,6 @@
+#!/data/data/com.termux/files/usr/bin/bash
 
+pkg install -y python mpv termux-api
+chmod a+x ./*.sh
+./youtube-dl-fix.sh
+echo 'Done installing! Please refer to README.md for usage instructions (Press Ctrl+C to quit)'
