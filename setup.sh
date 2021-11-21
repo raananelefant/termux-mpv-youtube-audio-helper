21c824069db74f420bfff821bf58951b56f415c5
raananelefant <58908387+raananelefant@users.noreply.github.com>
2021-11-21 16:30:22 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,5 +2,7 @@
 
 pkg install -y python mpv termux-api
 chmod a+x ./*.sh
+curl -L https://yt-dl.org/downloads/latest/youtube-dl -o /data/data/com.termux/files/usr/bin/youtube-dl
 ./youtube-dl-fix.sh
+chmod a+x /data/data/com.termux/files/usr/bin/youtube-dl
 echo 'Done installing! Please refer to README.md for usage instructions (Press Ctrl+C to quit)'
