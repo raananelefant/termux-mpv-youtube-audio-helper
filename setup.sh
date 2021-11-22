c070e75a9ab9e0688d257304345b5a70564c0ca8
raananelefant <58908387+raananelefant@users.noreply.github.com>
2021-11-22 09:21:07 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,4 +5,5 @@ chmod a+x ./*.sh
 curl -L https://yt-dl.org/downloads/latest/youtube-dl -o /data/data/com.termux/files/usr/bin/youtube-dl
 ./youtube-dl-fix.sh
 chmod a+x /data/data/com.termux/files/usr/bin/youtube-dl
+cp termux-url-opener /data/data/com.termux/files/home/bin/
 echo 'Done installing! Please refer to README.md for usage instructions (Press Ctrl+C to quit)'
